ed5afb03001577148e381e292065b14b7bfa36f8
Paul Mayorga <jpmayorga@outlook.com>
2020-07-19 19:23:00 -0500

    feat(frontend): adding material ui and react router dom

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+export function Routes() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
